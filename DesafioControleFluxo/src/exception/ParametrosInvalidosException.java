0084980867121376498853f6d00358eea81c00dd
mcnerirj <mcneri@gmail.com>
2023-08-30 19:48:10 -0300

    Comit Inicial do Desafio - Desvio de Fluxo

---
+++ DesafioControleFluxo/src/exception/ParametrosInvalidosException.java
@@ -0,0 +1,15 @@
+package exception;
+
+/**
+ * @author mcnerirj
+ */
+public class ParametrosInvalidosException extends Exception {
+    //Construtor vazio da classe
+    public ParametrosInvalidosException() {
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
